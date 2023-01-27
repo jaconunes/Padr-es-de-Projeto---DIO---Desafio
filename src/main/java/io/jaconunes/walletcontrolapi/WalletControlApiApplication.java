7d1d5091bf25070eb31e3968d4cc38a0830342e7
Jackson Nunes <jacksonnunes.bnu@gmail.com>
2023-01-26 21:36:10 -0300

    initial commit

---
+++ src/main/java/io/jaconunes/walletcontrolapi/WalletControlApiApplication.java
@@ -0,0 +1,13 @@
+package io.jaconunes.walletcontrolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletControlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletControlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/jaconunes/walletcontrolapi/WalletControlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jaconunes.walletcontrolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalletControlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
